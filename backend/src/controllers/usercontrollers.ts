5b90357c2e1422d3026f72ce6ed20a8e184f5097
Souvick Bhowmick <souvickb30@gmail.com>
2026-02-21 16:55:04 +0530

    Added hashing to the passwords

--- backend/src/controllers/usercontrollers.ts
+++ backend/src/controllers/usercontrollers.ts
@@ -1,5 +1,6 @@
 import { type Request,type Response,type NextFunction } from "express"
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
 import { USER } from "../models/userdb.js";
 import { env } from "../config/env.js";
@@ -21,17 +22,26 @@ const loginSchema = z.object({
 });
 
 export const signup = async(req: UpdatedReq,res: Response,next: NextFunction) => {
+    const saltRounds = 12;
     const {name,email,userid,password} = signupSchema.parse(req.body);
-    await USER.create({name,email,userid,password});
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    await USER.create({name,email,userid,password: hashedPassword});
     res.status(200).json({message: "Successful Sign-Up."});
 }
 
 export const login = async(req: UpdatedReq,res: Response,next: NextFunction) => {
     const {userid,password} = loginSchema.parse(req.body);
-    const user = await USER.findOne({userid,password});
+    const user = await USER.findOne({userid});
     if(!user) return res.status(401).json({message: "Wrong credentials"});
-    const token = jwt.sign({userid: user.userid},env.JWT_SECRET,{expiresIn: '2 days'});
-    console.log(token);
-    //res.cookie("token",token);
-    res.status(200).json({message: "Successful Log-In.", token: token});
+    try {
+        if(await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({userid: user.userid},env.JWT_SECRET,{expiresIn: '2 days'});
+            console.log(token);
+            //res.cookie("token",token);
+            res.status(200).json({message: "Successful Log-In.", token: token});
+        }
+    } catch (error) {
+        console.error(error)
+    }
+    
 }
